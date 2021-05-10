d57a5b8917af3b18f5f391dea2870fc4da34a530
AugustoSalgadoVG <83027404+AugustoSalgadoVG@users.noreply.github.com>
2021-05-09 21:07:14 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageInput();
+    }
+
+    void ManageInput()
+    {
+        
+        ManageMovement();
+        ManageShooting();
+    }
+
+    void ManageMovement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            transform.Translate(- velocity, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(velocity, 0, 0);
+        }
+    }
+
+    void ManageShooting()
+    {
+        
+    }
+}
